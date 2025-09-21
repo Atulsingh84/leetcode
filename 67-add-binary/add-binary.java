4ba83cea544bb9a10e6577d9f267d34cf552a953
Atul Kumar Singh <151540835+Atulsingh84@users.noreply.github.com>
2025-09-21 11:37:53 +0530

    Time: 2 ms (38.69%) | Memory: 42.5 MB (28.34%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -7,10 +7,15 @@ class Solution {
 
     while (i >= 0 || j >= 0 || carry == 1) 
     {
-      if(i >= 0)
-        carry += a.charAt(i--) - '0';
-      if(j >= 0)
-        carry += b.charAt(j--) - '0';
+      if(i >= 0){
+        carry += a.charAt(i) - '0';
+        i--;
+      }
+       
+      if(j >= 0){
+        carry += b.charAt(j) - '0';
+        j--;
+      }
       sb.append(carry % 2);
       carry /= 2;
     }
