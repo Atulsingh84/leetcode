0cb728e5d9d920bba3cddb2da5c1b137ac45e94e
Atul Kumar Singh <151540835+Atulsingh84@users.noreply.github.com>
2025-09-26 22:33:08 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (51.07%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int climbStairs(int n) {
+    //     if(n==0 || n==1){
+    //         return 1;
+    //     }
+    //   return climbStairs(n-1) + climbStairs(n-2);
+
+    if(n <= 1) return 1;
+
+
+    int first = 1;
+    int second = 1;
+    int result =0;
+
+    for(int i=2;i<=n;i++){
+        result = first+second;
+        first = second;
+        second = result;
+    }
+    return result;
+    }
+}
