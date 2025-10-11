6a885bf79310f4db38bc05930001234470b4aeeb
Atul Kumar Singh <151540835+Atulsingh84@users.noreply.github.com>
2025-10-11 23:36:01 +0530

    Time: 30 ms (70.96%) | Memory: 51.4 MB (70.13%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -6,6 +6,8 @@ class Solution {
         // Sort the array
         Arrays.sort(nums);
 
+
+        //Fixing One Element
         for (int i = 0; i < nums.length - 2; i++) {
             // Skip duplicate elements for i
             if (i > 0 && nums[i] == nums[i - 1]) {
