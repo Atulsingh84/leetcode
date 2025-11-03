f7ab02603c9226bdfbb8b872106925023e2e475c
Atul Kumar Singh <151540835+Atulsingh84@users.noreply.github.com>
2025-11-03 23:12:42 +0530

    Time: 8 ms (5.64%) | Memory: 45 MB (6.13%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -3,37 +3,25 @@ class Solution {
      Arrays.sort(nums1);
         Arrays.sort(nums2);
         
-        Set<Integer> intersectionSet = new HashSet<>();
-        
-        // 2. Use two pointers to iterate through both arrays
-        int i = 0; // Pointer for nums1
-        int j = 0; // Pointer for nums2
-        
-        while (i < nums1.length && j < nums2.length) {
-            if (nums1[i] < nums2[j]) {
-                // nums1's element is smaller, move its pointer
-                i++;
-            } else if (nums1[i] > nums2[j]) {
-                // nums2's element is smaller, move its pointer
-                j++;
-            } else {
-                // They are equal! This is an intersection.
-                intersectionSet.add(nums1[i]);
-                // Move both pointers
-                i++;
-                j++;
+        Set<Integer> set1 = new HashSet<>();
+
+        for(int n: nums1){
+            set1.add(n);
+        }
+
+        Set<Integer> Intersection = new HashSet<>();
+
+        for(int n : nums2){
+            if(set1.contains(n)){
+                Intersection.add(n);
             }
         }
         
-        // 3. Convert the set to an array
-        int[] result = new int[intersectionSet.size()];
-        int k = 0;
-        for (int num : intersectionSet) {
-            result[k] = num;
-            k++;
+        int[] Result = new int[Intersection.size()];
+        int i =0;
+        for(int num : Intersection){
+            Result[i++] = num;
         }
-        
-        return result;
-
+       return Result;
     }
 }
