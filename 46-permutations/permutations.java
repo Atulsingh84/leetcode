a7d79e7e3ecc5554b2346188863e6ef62b9d7d5f
Atul Kumar Singh <151540835+Atulsingh84@users.noreply.github.com>
2025-11-06 23:21:53 +0530

    Time: 1 ms (92.79%) | Memory: 45.3 MB (6.34%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -8,24 +8,24 @@ class Solution {
     }
 
     private void backtrack(int[] nums, List<Integer> current, List<List<Integer>> result) {
-        // If the current list length equals nums length, we have a complete permutation
+
         if (current.size() == nums.length) {
-            result.add(new ArrayList<>(current)); // add a COPY of current list
+            result.add(new ArrayList<>(current) );
             return;
         }
 
-        // Try placing each number in the next position
+        
         for (int num : nums) {
-            // If the number is already used, skip it
+           
             if (current.contains(num)) continue;
 
-            // Choose the number
+           
             current.add(num);
 
-            // Recurse to build the rest of the permutation
+          
             backtrack(nums, current, result);
 
-            // Undo the choice (Backtrack)
+          
             current.remove(current.size() - 1);
         }
     }
