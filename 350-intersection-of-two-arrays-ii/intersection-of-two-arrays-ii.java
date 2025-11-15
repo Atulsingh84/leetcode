7f777c1d5f43865863f785d92b732399ba49ca1b
Atul Kumar Singh <151540835+Atulsingh84@users.noreply.github.com>
2025-11-15 13:05:13 +0530

    Time: 9 ms (5.77%) | Memory: 45.2 MB (6.12%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -1,29 +1,45 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
         
-         Map<Integer, Integer> map = new HashMap<>();
+        //  Map<Integer, Integer> map = new HashMap<>();
 
-        // Count frequency of nums1
-        for (int num : nums1) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        // // Count frequency of nums1
+        // for (int num : nums1) {
+        //     map.put(num, map.getOrDefault(num, 0) + 1);
+        // }
+
+        // List<Integer> resultList = new ArrayList<>();
+
+        // // Check nums2 against map
+        // for (int num : nums2) {
+        //     if (map.containsKey(num) && map.get(num) > 0) {
+        //         resultList.add(num);
+        //         map.put(num, map.get(num) - 1); // reduce count
+        //     }
+        // }
+
+        // // Convert List to int[]
+        // int[] result = new int[resultList.size()];
+        // for (int i = 0; i < resultList.size(); i++) {
+        //     result[i] = resultList.get(i);
+        // }
+
+        // return result;
+
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0, j = 0;
+        List<Integer> res = new ArrayList<>();
+
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                res.add(nums1[i]);
+                i++; j++;
+            } else if (nums1[i] < nums2[j]) i++;
+            else j++;
         }
 
-        List<Integer> resultList = new ArrayList<>();
+        return res.stream().mapToInt(x -> x).toArray();
 
-        // Check nums2 against map
-        for (int num : nums2) {
-            if (map.containsKey(num) && map.get(num) > 0) {
-                resultList.add(num);
-                map.put(num, map.get(num) - 1); // reduce count
-            }
-        }
-
-        // Convert List to int[]
-        int[] result = new int[resultList.size()];
-        for (int i = 0; i < resultList.size(); i++) {
-            result[i] = resultList.get(i);
-        }
-
-        return result;
     }
 }
